1ea2bd5a0da97dcaeb8e1fee51db2d70e90b74d6
vuslan <vuslan^@yahoo.com>
2019-07-16 05:36:17 +0000

    delete step01 & modify tsksvm

--- tsksvm_step01.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python2.7
-import tsksvm
-# import pdb; pdb.set_trace()
-
-# PREPROCESS AA SCALES
-
-# min-max normalized from coepra-scales-2.csv
-scales_file = "data/coepra-scales-2.csv"  # scales(=643)xaa(=20)
-scales = tsksvm.preprocess_aa_scales(scales_file)
