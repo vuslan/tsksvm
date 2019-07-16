5135b74a6c50dfd0f720f23bbcc1f9d2b5b09b2a
vuslan <vuslan^@yahoo.com>
2019-07-16 05:55:39 +0000

    delete step02

--- tsksvm_step02.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python2.7
-#tsksvm_step01: PREPROCESS AA SCALES
-from tsksvm_step01 import *
-
-# DEFAULT SELECT MODEL PARAMETERS
-
-# init: cnum=2, svrc=1.0, svrp=0.05
-sm = tsksvm.SelectModel(cnum=2, svrc=1.0, svrp=0.05)
